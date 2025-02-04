1ced84910d706c2a09dfaea75f48419a726ad563
hctel <hugo.de.hepcee@gmail.com>
2025-02-04 10:55:05 +0100

    Added database connector

---
+++ serverapp/DatabaseConnector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
+
+namespace backend
+{
+    public class DatabaseConnector
+    {
+        private MySqlConnection connection;
+        public DatabaseConnector(string server, string databaseName, string username, string password)
+        {
+            string connstring = string.Format("Server={0}; database={1}; UID={2}; password={3}", server, databaseName, username, password);
+            connection = new MySqlConnection(connstring);
+            connection.Open();
+        }
+
+        public bool isOpen()
+        {
+            return connection.State == System.Data.ConnectionState.Open;
+        }
+
+        public MySqlDataReader read(string query) {
+            return new MySqlCommand(query, connection).ExecuteReader();
+        }
+
+        public bool execute(string query)
+        {
+            return new MySqlCommand(query, connection).ExecuteNonQuery() != 0;
+        }
+
+        
+    }
+}
+
