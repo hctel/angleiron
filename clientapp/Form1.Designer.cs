cddf9113a16a5693dc69ee902a9e9eecc6d4f68e
Irintsoanom <22336@ecam.be>
2025-02-04 11:39:46 +0100

    Show title

--- clientapp/Form1.Designer.cs
+++ clientapp/Form1.Designer.cs
@@ -29,15 +29,30 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
-            this.panel1.Location = new System.Drawing.Point(1, -2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1, -10);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1573, 158);
+            this.panel1.Size = new System.Drawing.Size(1573, 166);
             this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(11, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(369, 85);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AngleIron";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
@@ -48,6 +63,8 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -55,6 +72,7 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- clientapp/Form1.cs
+++ clientapp/Form1.cs
@@ -21,5 +21,10 @@ namespace clientapp
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            la
+        }
     }
 }
