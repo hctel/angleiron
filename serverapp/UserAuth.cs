34a11e3739d683e90363ebae3eba85180e491a96
hctel <hugo.de.hepcee@gmail.com>
2025-02-04 11:10:17 +0100

    Fix project import

---
+++ serverapp/UserAuth.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class UserAuth
+{
+	public string SessionId { get; }
+	public int UserId { get; }
+	public string RemoteIp { get; }
+    public UserAuth(int userid, string remoteIp)
+	{
+		SessionId = Guid.NewGuid().ToString();
+        UserId = userid;
+        RemoteIp = remoteIp;
+    }
+}
