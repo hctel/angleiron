6aed46b27533598e4cf802cd84fae24c2cc1948b
Hugo de Hepcée <22167@ecam.be>
2025-02-05 11:35:56 +0100

    Started network handler class

---
+++ serverapp/Network.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace backend
+{
+	public class Network
+	{
+		IPEndPoint endPoint;
+		TcpListener listener;
+        public Network(int port)
+		{
+			if (port < 1) throw new ArgumentOutOfRangeException("Port must be greater than 1");
+            endPoint = new IPEndPoint(IPAddress.Any, port);
+            listener = new TcpListener(endPoint);
+        }
+	}
+}
