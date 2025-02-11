7b21f9af7a8b59c5046bf0eada2c0b167bc685fc
hctel <hugo.de.hepcee@gmail.com>
2025-02-11 09:29:44 +0100

    Implemented User authentication

--- serverapp/Program.cs
+++ serverapp/Program.cs
@@ -6,8 +6,13 @@ int port = 6210;
 
 Console.WriteLine($"AngleIron server v{version}");
 Console.WriteLine("Starting server...");
-Network networkManager = new Network(port);
-//...
+
+UserDB usersDatabase = new UserDB("localhost", "angleiron", "root", "root"); //CHANGE CREDENTIALS HERE
+
+UserAuth userAuthenticator = new UserAuth(usersDatabase);
+
+
+Network networkManager = new Network(port, networkReceiveFunction); //TO DO LASTLY!!
 Console.WriteLine($"Server started on port {port}");
 Console.WriteLine("Press Q to stop the server");
 
@@ -21,3 +26,23 @@ for (; ; )
 
 Console.WriteLine("Stopping server...");
 networkManager.kill();
+
+string networkReceiveFunction(string[] data)
+{
+    if (data[0].Equals("AUTH"))
+    {
+        try
+        {
+            return "AUTHOK&" + userAuthenticator.authUser(data[1], data[2]).ToString();
+        }
+        catch (KeyNotFoundException e)
+        {
+            return "AUTHFAIL&NOUSER";
+        }
+        catch (Exception e)
+        {
+            return "AUTHFAIL&NOPASSWD";
+        }
+    }
+    return "NOFUNC";
+}
