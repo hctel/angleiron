abd78943b8d38edb773fecdb086ea9d346d6f2aa
21242NS <nicolasschell234@gmail.com>
2025-02-11 11:06:42 +0100

    Refix de stock_calculation

--- serverapp/Stock_calculation.cs
+++ serverapp/Stock_calculation.cs
@@ -2,13 +2,30 @@ using MySql.Data.MySqlClient;
 using System;
 
 namespace backend {
-    public class Stock_calculation : DatabaseConnector
+    public class Stock_calculation 
     {
+        private Stock_DB database;
         private int not_in_stock;
         private int to_order;
-        public Stock_calculation(string server, string databaseName, string username, string password):base(server, databaseName, username, password)
+        public Stock_calculation(Stock_DB database)
         {
-            
+            this.database=database;
+        }
+        public void check(int id_composant){
+            MySqlDataReader result = database.getIdcomposant(id_composant);
+            if(result.Read()){
+                int delta_stock_need=0;
+                delta_stock_need = result.GetInt32("Quantité")-result.GetInt32("Quantité_client");
+                not_in_stock = delta_stock_need;
+                delta_stock_need += result.GetInt32("Quantité_commandée");
+                to_order = delta_stock_need;
+            }
+        }
+        public int get_not_in_stock(){
+            return not_in_stock;
+        }
+        public int get_to_order(){
+            return to_order;
         }
     }
 }
