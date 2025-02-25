b27b497c6101027dd66d54177738c82ee52fbeb1
21242NS <nicolasschell234@gmail.com>
2025-02-25 18:17:39 +0100

    update try to have less error

--- serverapp/Order_management.cs
+++ serverapp/Order_management.cs
@@ -4,117 +4,177 @@ using MySqlX.XDevAPI.Common;
 using Org.BouncyCastle.Asn1.Misc;
 using System;
 
-namespace backend {
-    public class Order_management {
+namespace backend
+{
+    public class Order_management
+    {
         private Order_DB order_DB;
         private Stock_DB stock_DB;
         private Stock_calculation stock_calculation;
         private MaterialDB materialDB;
         private KIT_to_component kIT_To_Component;
-        public Order_management(Order_DB order_DB, Stock_DB stock_DB, KitDB kitDB, MaterialDB materialDB, KIT_to_component kIT_To_Component, Stock_calculation stock_calculation){ 
-            this.order_DB=order_DB;
-            this.stock_DB=stock_DB;
-            this.kIT_To_Component=kIT_To_Component;
-            this.materialDB=materialDB;
-            this.stock_calculation=stock_calculation;
+        public Order_management(Order_DB order_DB, Stock_DB stock_DB, KitDB kitDB, MaterialDB materialDB, KIT_to_component kIT_To_Component, Stock_calculation stock_calculation)
+        {
+            this.order_DB = order_DB;
+            this.stock_DB = stock_DB;
+            this.kIT_To_Component = kIT_To_Component;
+            this.materialDB = materialDB;
+            this.stock_calculation = stock_calculation;
         }
-        public void management(int idorder){
-            using(MySqlDataReader resultOrder = order_DB.getIdOrder(idorder)){
-                resultOrder.Read();
-                int id_category = resultOrder.GetInt32("id_category");
-                using(MySqlDataReader resultcomponent_type = kIT_To_Component.getIdcategory(id_category)){
-                while (resultcomponent_type.Read()) {
-                    int id_component = resultcomponent_type.GetInt32("id_component");
-                    using(MySqlDataReader resultStock = stock_DB.getIdcomponent(id_component)){
-                        resultStock.Read();
-                        int newQuantiyclient = resultStock.GetInt32("Quantity_client")+resultcomponent_type.GetInt32("numberpercategory");
-                        stock_DB.updateINT("Quantity_client", newQuantiyclient, id_component); 
+        public void management(int idorder)
+        {
+            try
+            {
+                using (MySqlDataReader resultOrder = order_DB.getIdOrder(idorder))
+                {
+                    if (resultOrder.HasRows && resultOrder.Read())
+                    {
+                        int id_category = resultOrder.GetInt32("id_category");
+
+                        using (MySqlDataReader resultcomponent_type = kIT_To_Component.getIdcategory(id_category))
+                        {
+                            while (resultcomponent_type.Read())
+                            {
+                                int id_component = resultcomponent_type.GetInt32("id_component");
+
+                                using (MySqlDataReader resultStock = stock_DB.getIdcomponent(id_component))
+                                {
+                                    if (resultStock.HasRows && resultStock.Read())
+                                    {
+                                        int quantityClient = resultStock.GetInt32("Quantity_client");
+                                        int numberPerCategory = resultcomponent_type.GetInt32("numberpercategory");
+                                        int newQuantityClient = quantityClient + numberPerCategory;
+
+                                        stock_DB.updateINT("Quantity_client", newQuantityClient, id_component);
+                                    }
+                                }
+                            }
                         }
-                    } 
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Aucune commande trouvée avec l'ID {idorder}.");
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erreur lors du traitement de l'ordre {idorder} : {ex.Message}");
+            }
         }
-        public void add_order(int idcategory, int id_client, string already_paid, string status, double price){
-            order_DB.addOrder(idcategory, id_client,already_paid, status, price);
+
+        public void add_order(int idcategory, int id_client, string already_paid, string status, double price)
+        {
+            order_DB.addOrder(idcategory, id_client, already_paid, status, price);
         }
-        public string get_status(int idorder){
+        public string get_status(int idorder)
+        {
             string result;
-            using(MySqlDataReader row = order_DB.getIdOrder(idorder)){
+            using (MySqlDataReader row = order_DB.getIdOrder(idorder))
+            {
                 row.Read();
                 result = row.GetString("status");
             }
             return result;
         }
-        public string get_already_paid(int idorder){
+        public string get_already_paid(int idorder)
+        {
             string result;
-            using(MySqlDataReader row = order_DB.getIdOrder(idorder)){
+            using (MySqlDataReader row = order_DB.getIdOrder(idorder))
+            {
                 row.Read();
                 result = row.GetString("Already_paid");
             }
             return result;
         }
-        public List<List<string>> get_orders(){
+        public List<List<string>> get_orders()
+        {
+
             List<List<string>> result = new List<List<string>>();
-            using(MySqlDataReader row = order_DB.getOrder()){
-                while(row.Read()){
-                    List<string> row_result = new List<string>();
-                    row_result.Add(row.GetInt32("idorder") + "");
-                    row_result.Add(row.GetInt32("id_client") + "");
-                    row_result.Add(row.GetInt32("id_category") + "");
-                    row_result.Add(row.GetDouble("Price") + "");
-                    row_result.Add(row.GetString("Already_paid"));
-                    row_result.Add(row.GetString("Status"));
-                    result.Add(row_result);
+
+            try
+            {
+                using (MySqlDataReader row = order_DB.getOrder())
+                {
+                    if (row.HasRows)
+                    {
+                        while (row.Read())
+                        {
+                            List<string> row_result = new List<string>();
+
+                            row_result.Add(row.GetInt32("idorder") + "");
+                            row_result.Add(row.GetInt32("id_client") + "");
+                            row_result.Add(row.GetInt32("id_category") + "");
+                            row_result.Add(row.GetDouble("Price") + "");
+                            row_result.Add(row.GetString("Already_paid"));
+                            row_result.Add(row.GetString("Status"));
+                            result.Add(row_result);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Aucune commande trouvée.");
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erreur lors de la récupération des commandes : {ex.Message}");
+            }
+
             return result;
         }
-        public List<List<string>> detail_order(int idorder){
+
+
+        public List<List<string>> detail_order(int idorder)
+        {
             List<List<string>> result = new List<List<string>>();
-            using(MySqlDataReader row = order_DB.getIdOrder(idorder)){
+            using (MySqlDataReader row = order_DB.getIdOrder(idorder))
+            {
                 row.Read();
                 int id_category = row.GetInt32("id_category");
-                using(MySqlDataReader resultcomponent_type = kIT_To_Component.getIdcategory(id_category)){
-                    while (resultcomponent_type.Read()) {
+                using (MySqlDataReader resultcomponent_type = kIT_To_Component.getIdcategory(id_category))
+                {
+                    while (resultcomponent_type.Read())
+                    {
                         List<string> row_result = new List<string>();
                         int id_component = resultcomponent_type.GetInt32("id_component");
                         row_result.Add(id_component + "");
-                        using(MySqlDataReader resultStock = stock_DB.getIdcomponent(id_component)){
+                        using (MySqlDataReader resultStock = stock_DB.getIdcomponent(id_component))
+                        {
                             resultStock.Read();
                             row_result.Add(resultStock.GetInt32("Quantity_client") + "");
                             int not_in_stock = stock_calculation.get_not_in_stock();
-                            if(not_in_stock>0){
+                            if (not_in_stock > 0)
+                            {
                                 row_result.Add("1");
                             }
-                            else{
+                            else
+                            {
                                 row_result.Add("0");
                             }
                         }
-                        using(MySqlDataReader material = materialDB.getIdcomponent(id_component)){
+                        using (MySqlDataReader material = materialDB.getIdcomponent(id_component))
+                        {
                             material.Read();
                             row_result.Add(material.GetString("Description"));
                             result.Add(row_result);
                         }
-                    }   
+                    }
                 }
             }
-            
-            
+
+
             return result;
         }
-        public void change_satus(string new_satus, int idorder){
+        public void change_satus(string new_satus, int idorder)
+        {
             order_DB.updateSTR("Status", new_satus, idorder);
         }
-        public void delete_row(int idorder){
+        public void delete_row(int idorder)
+        {
             order_DB.delete_row(idorder);
         }
 
     }
-    
-
-
-
-
-
-
 }
