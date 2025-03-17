02d90aeb04e8e2ca15bfa1c6295248269d2d73a4
Deuvaert Victor <22054@ecam.be>
2025-03-17 10:54:37 +0100

    Verify inStock

--- merchandapp/Form1.cs
+++ merchandapp/Form1.cs
@@ -143,6 +143,14 @@ namespace merchandapp
         {
             network.updateStatus(currentOrder.orderID);
             Debug.WriteLine(currentOrder.orderID);
+
+            index = 0;
+            orderSummariesSource.Clear();
+            foreach (OrderSummary order in network.getOrders())
+            { orderSummariesSource.Add(order); }
+            listPanel[index].BringToFront();
+            orderPartsSource.Clear();
+            //((Button)sender).Text = index.ToString();
         }
     }
 }
