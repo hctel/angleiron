a1d32b21b2dccd9f07fe67b6ec37c3a2fbb2123a
Irintsoanom <22336@ecam.be>
2025-03-21 13:37:32 +0100

    Adding color buttons

--- ClientAppRemake/Form1.cs
+++ ClientAppRemake/Form1.cs
@@ -68,6 +68,44 @@ namespace ClientAppRemake
             previewPanel.BackColor = Color.FromArgb(255, 255, 255);
             previewPanel.Dock = DockStyle.Fill;
             mainPanel.Controls.Add(previewPanel);
+
+            //Color button
+            Button choiceOneButton = new Button();
+            choiceOneButton.Size = new Size(50, 50);
+            choiceOneButton.Location = new Point(0, 0);
+            choiceOneButton.FlatStyle = FlatStyle.Flat;
+            choiceOneButton.FlatAppearance.BorderSize = 0;
+            choiceOneButton.BackColor = Color.FromArgb(187, 153, 112);
+            Button choiceTwoButton = new Button();
+            choiceTwoButton.Size = new Size(50, 50);
+            choiceTwoButton.Location = new Point(50, 0);
+            choiceTwoButton.FlatStyle = FlatStyle.Flat;
+            choiceTwoButton.FlatAppearance.BorderSize = 0;
+            choiceTwoButton.BackColor = Color.FromArgb(88, 76, 69);
+            Button choiceThreeButton = new Button();
+            choiceThreeButton.Size = new Size(50, 50);
+            choiceThreeButton.Location = new Point(100, 0);
+            choiceThreeButton.FlatStyle = FlatStyle.Flat;
+            choiceThreeButton.FlatAppearance.BorderSize = 0;
+            choiceThreeButton.BackColor = Color.FromArgb(191, 180, 157);
+            Button choiceFourButton = new Button();
+            choiceFourButton.Size = new Size(50, 50);
+            choiceFourButton.Location = new Point(150, 0);
+            choiceFourButton.FlatStyle = FlatStyle.Flat;
+            choiceFourButton.FlatAppearance.BorderSize = 0;
+            choiceFourButton.BackColor = Color.FromArgb(166, 167, 171);
+
+            //Color Group
+            GroupBox colorGroup = new GroupBox();
+            colorGroup.BackColor = Color.FromArgb(200, 200, 200);
+            colorGroup.FlatStyle = FlatStyle.Flat;
+            colorGroup.Size = new Size(360,75);
+            colorGroup.Controls.Add(choiceOneButton);
+            colorGroup.Controls.Add(choiceTwoButton);
+            colorGroup.Controls.Add(choiceThreeButton);
+            colorGroup.Controls.Add(choiceFourButton);
+            colorGroup.Location = new Point((selectionPanel.Width - colorGroup.Width) / 2, selectionPanel.Height - 200);
+            selectionPanel.Controls.Add(colorGroup);
         }
     }
 }
