e5c2a8fdb67f12d6256231a110cf2d4af0e54c9d
Irintsoanom <22336@ecam.be>
2025-03-21 13:09:39 +0100

    Finalizing stucture of mainPanel

--- ClientAppRemake/Form1.cs
+++ ClientAppRemake/Form1.cs
@@ -53,15 +53,21 @@ namespace ClientAppRemake
             Panel selectionPanel = new Panel();
             selectionPanel.Size = new Size(450, mainPanel.Height);
             selectionPanel.BackColor = Color.FromArgb(0, 0, 232);
-            selectionPanel.Location = new Point(0, 0); 
+            selectionPanel.Dock = DockStyle.Left;
             mainPanel.Controls.Add(selectionPanel);
 
             //Summary Panel
             Panel summaryPanel = new Panel();
             summaryPanel.Size = new Size(450, mainPanel.Height);
             summaryPanel.BackColor = Color.FromArgb(0, 232, 0);
-            summaryPanel.Location = new Point(mainPanel.Width - summaryPanel.Width, 0);
+            summaryPanel.Location = new Point(mainPanel.Width - summaryPanel.Width - 25, 0);
             mainPanel.Controls.Add(summaryPanel);
+
+            //Preview Panel
+            Panel previewPanel = new Panel();
+            previewPanel.BackColor = Color.FromArgb(255, 255, 255);
+            previewPanel.Dock = DockStyle.Fill;
+            mainPanel.Controls.Add(previewPanel);
         }
     }
 }
