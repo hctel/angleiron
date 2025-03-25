5f5eb43998740c690a4dc488ae2100e62426a01d
Irintsoanom <22336@ecam.be>
2025-03-25 10:11:00 +0100

    Resize test

--- ClientAppRemake/Form1.cs
+++ ClientAppRemake/Form1.cs
@@ -5,9 +5,9 @@ namespace ClientAppRemake
         public Form1()
         {
             InitializeComponent();
-            this.Size = new Size(1920, 1080);
-            this.MinimumSize = new Size(1920, 1080);
-            this.MaximumSize = new Size(1920, 1080);
+            this.Size = new Size(1600, 900);
+            this.MinimumSize = new Size(1600, 900);
+            this.MaximumSize = new Size(1600, 900);
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
             this.StartPosition = FormStartPosition.CenterScreen;
             this.Text = "AngleIron V2";
@@ -22,7 +22,7 @@ namespace ClientAppRemake
         {
             //Header Panel
             Panel headerPanel = new Panel();
-            headerPanel.Size = new Size(1920, 160);
+            headerPanel.Size = new Size(1600, 160);
             headerPanel.BackColor = Color.FromArgb(0, 168, 232);
             headerPanel.Location = new Point(0, 0);
             this.Controls.Add(headerPanel);
@@ -38,14 +38,14 @@ namespace ClientAppRemake
 
             //Footer Panel
             Panel footerPanel = new Panel();
-            footerPanel.Size = new Size(1920, 160);
+            footerPanel.Size = new Size(1600, 160);
             footerPanel.BackColor = Color.FromArgb(0, 168, 232);
             footerPanel.Location = new Point(0, this.ClientSize.Height - footerPanel.Height);
             this.Controls.Add(footerPanel);
 
             //Main Panel
             Panel mainPanel = new Panel();
-            mainPanel.Size = new Size(1920, 1080 - headerPanel.Height - footerPanel.Height);
+            mainPanel.Size = new Size(1600, 900 - headerPanel.Height - footerPanel.Height);
             mainPanel.BackColor = Color.FromArgb(255, 255, 255);
             mainPanel.Location = new Point(0, headerPanel.Height);
             this.Controls.Add(mainPanel);
