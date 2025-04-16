ebf939da5647f4e39700ace1a3a105ba88262b39
Irintsoanom <22336@ecam.be>
2025-04-16 15:38:52 +0200

    Solving autoscale problem

--- ClientAppRemake/Program.cs
+++ ClientAppRemake/Program.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using clientapp;
+using System.Runtime.InteropServices;
 
 
 namespace ClientAppRemake
 {
     internal static class Program
     {
+        [DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
         public static Network network;
         /// <summary>
         /// Point d'entrée principal de l'application.
@@ -17,6 +20,9 @@ namespace ClientAppRemake
         [STAThread]
         static void Main()
         {
+            if (Environment.OSVersion.Version.Major >= 6)
+                SetProcessDPIAware();
+
             network = new Network("hctel.net",58931);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
