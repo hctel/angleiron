491124896f166f1a733d97062b2b09e4e2c4c2ac
Irintsoanom <22336@ecam.be>
2025-04-16 16:48:49 +0200

    Rescale again

--- ClientAppRemake/Form1.cs
+++ ClientAppRemake/Form1.cs
@@ -66,7 +66,7 @@ namespace ClientAppRemake
 
             //Header Panel
             Panel headerPanel = new Panel();
-            headerPanel.Size = new Size(1600, 100);
+            headerPanel.Size = new Size(1920, 120);
             headerPanel.BackColor = Color.FromArgb(0, 168, 232);
             headerPanel.Location = new Point(0, 0);
             this.Controls.Add(headerPanel);
@@ -82,21 +82,21 @@ namespace ClientAppRemake
 
             //Footer Panel
             Panel footerPanel = new Panel();
-            footerPanel.Size = new Size(1600, 100);
+            footerPanel.Size = new Size(1920, 150);
             footerPanel.BackColor = Color.FromArgb(0, 168, 232);
             footerPanel.Location = new Point(0, this.ClientSize.Height - footerPanel.Height);
             this.Controls.Add(footerPanel);
 
             //Main Panel
             Panel mainPanel = new Panel();
-            mainPanel.Size = new Size(1600, 900 - headerPanel.Height - footerPanel.Height);
+            mainPanel.Size = new Size(1920, 1080 - headerPanel.Height - footerPanel.Height);
             mainPanel.BackColor = Color.FromArgb(255, 255, 255);
             mainPanel.Location = new Point(0, headerPanel.Height);
             this.Controls.Add(mainPanel);
 
             //Selection Panel
             Panel selectionPanel = new Panel();
-            selectionPanel.Size = new Size(350, mainPanel.Height);
+            selectionPanel.Size = new Size(450, mainPanel.Height);
             selectionPanel.BackColor = Color.FromArgb(200, 200, 200);
             selectionPanel.Dock = DockStyle.Left;
             mainPanel.Controls.Add(selectionPanel);
@@ -111,7 +111,7 @@ namespace ClientAppRemake
 
             //Summary Panel
             Panel summaryPanel = new Panel();
-            summaryPanel.Size = new Size(350, mainPanel.Height);
+            summaryPanel.Size = new Size(450, mainPanel.Height);
             summaryPanel.BackColor = Color.FromArgb(0, 232, 0);
             summaryPanel.Location = new Point(mainPanel.Width - summaryPanel.Width - 25, 0);
             mainPanel.Controls.Add(summaryPanel);
@@ -286,7 +286,7 @@ namespace ClientAppRemake
                 }
             }
             //Item panel ends here
-
+            
             //Preview image
             
             this.previewImage.Size = new Size(600, 600);
